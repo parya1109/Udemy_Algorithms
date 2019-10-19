21594b4f85d07b264c24e78cadb3e3ce2d99d1ad
parthiv <parthiv2eng@gmail.com>
2019-10-19 16:14:50 -0700

    - Initial Commit

---
+++ Practice/Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Parthiv Prajapati on 10/19/19.
+//  Copyright © 2019 Parthiv Prajapati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
